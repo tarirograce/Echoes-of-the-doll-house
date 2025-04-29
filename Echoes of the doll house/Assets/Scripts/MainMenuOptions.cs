6e929dc4f525da5a9deef5d8cde0336f03e5e0e5
Swornashabi-Shiwakoti <swornashabi.shiwakoti@mnsu.edu>
2025-04-29 12:47:12 -0500

    MenuChanges

---
+++ Echoes of the doll house/Assets/Scripts/MainMenuOptions.cs
@@ -0,0 +1,14 @@
+// Written by Swornashabi
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuOptions : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
